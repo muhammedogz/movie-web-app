3c8a2d2a9354c9a246eff9685b0272e2e0b4248f
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2021-11-08 01:55:58 +0300

    Add footer component and style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Footer from './components/Footer';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
         <h1>Hi</h1>
+        <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <footer className='Footer'>
+            <h1 className='Footer-title'>Hello from footer</h1>
+        </footer>
+    )
+}
+
+export default Footer
