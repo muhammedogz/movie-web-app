59bc673c697644ac8eb5f34cd08be7f753fe0228
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2021-11-08 22:54:14 +0300

    Add demo function and change styyle

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,11 @@
+// import { useEffect } from "react";
 import "./Movie.css"
 import MoviePhoto from "../images/hero.jpg"
 
-const Movie = () => {
+const Movie = (props) => {
+    console.log(props);
     return (
-        <div className="Movie">
+        <div className="Movie" onClick={props.onclick}>
             <p className="Movie-score">Score:5</p>
             <img className="Movie-img" src={MoviePhoto} alt="Movie info"></img>
             <p className="Movie-name">Movie Name</p>
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,19 +2,29 @@ import Movie from './Movie'
 import "./Movies.css"
 
 const Movies = () => {
+    const saySomething = (something) => {
+        console.log(something);
+      };
+      
+      const click = (e) => {
+          console.log(`e:`, e)
+        saySomething("element clicked");
+      };
+
     return (
         <div className="Movies container">
             <h2>Popular Movies</h2>
             <div className="Movies-all">
-                <Movie />
-                <Movie />
-                <Movie />
-                <Movie />
-                <Movie />
-                <Movie />
-                <Movie />
-                <Movie />
-                <Movie />
+                <Movie onclick={click}  />
+                <Movie onclick={click}  />
+                <Movie onclick={click}  />
+                <Movie onclick={click}  />
+                <Movie onclick={click}  />
+                <Movie onclick={click}  />
+                <Movie onclick={click}  />
+                <Movie onclick={click}  />
+                <Movie onclick={click}  />
+
             </div>
         </div>
     )
