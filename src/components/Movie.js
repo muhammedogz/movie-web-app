45d4e14dda05c28be6596573fc73f8b737eb834a
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2021-11-09 15:38:04 +0300

    Move score info to left

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,7 +6,7 @@ const Movie = (props) => {
     console.log(props);
     return (
         <div className="Movie">
-            <p className="Movie-score">Score:{movie.vote_average}</p>
+            <p className="Movie-score">{movie.vote_average}</p>
             <img className="Movie-img" src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path} alt="Movie info"></img>
             <p className="Movie-name">{movie.title}</p>
         </div>
