52cdb3ca8d1a072f94c8fad03efd97aef7ad753d
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2021-11-09 15:55:48 +0300

    Add bg img adding style

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,11 +3,12 @@ import "./Movie.css"
 
 const Movie = (props) => {
     const movie = props.movie;
-    console.log(props);
+    const img = "https://image.tmdb.org/t/p/w500/" + movie.poster_path;
+    // style={{backgroundImage:`url(${img})`}} 
     return (
         <div className="Movie">
             <p className="Movie-score">{movie.vote_average}</p>
-            <img className="Movie-img" src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path} alt="Movie info"></img>
+            <img className="Movie-img" src={img} alt={movie.overview}></img>
             <p className="Movie-name">{movie.title}</p>
         </div>
     )
